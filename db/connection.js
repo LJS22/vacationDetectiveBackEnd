209490880811d9c616f9674852827fb7e8def3be
LJS22 <LJS22@users.noreply.github.com>
2020-09-02 13:57:59 +0100

    added database connection

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const pass = process.env.DB_PASS;
 const dbName = process.env.DB_NAME;
 const dbCluster = process.env.DB_CLUSTER;
 
-const mongoDB = `url`;
+const mongoDB = `mongodb+srv://${user}:${pass}@${dbCluster}/${dbName}?retryWrites=true&w=majority`;
 
 mongoose.connect(mongoDB, {
   useNewUrlParser: true,
