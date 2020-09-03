88251ec85d58b62908f1721af43cf709c6426b0a
LJS22 <LJS22@users.noreply.github.com>
2020-09-03 16:40:03 +0100

    phwaaa

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,17 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
-exports.addUser = function (req, res, next) {
-  const { firstName, lastName, userName, email, password } = req.body;
+exports.addUser = async function (req, res, next) {
+  let { firstName, lastName, userName, email, password } = req.body;
+
+  const hash = await bcrypt.hashSync(password, 10);
 
   const newUser = new User({
     firstName,
     lastName,
     userName,
     email,
-    password,
+    password: hash,
   });
 
   newUser.save((err) => {
@@ -16,6 +19,7 @@ exports.addUser = function (req, res, next) {
     else
       res.status(201).json({
         message: { msgBody: "User successfully added", msgError: false },
+        data: newUser,
       });
   });
 };
@@ -31,22 +35,58 @@ exports.returnAllUsers = function (req, res, next) {
   });
 };
 
-sendData = async (e) => {
-  const response = await fetch(
-    "http://localhost:4500/vacationdetective/v1/addUser",
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        firstName: this.state.firstName,
-        lastName: this.state.lastName,
-        userName: this.state.userName,
-        email: this.state.email,
-        password: this.state.password,
-      }),
+// exports.signInAuthentication = function (req, res, next) {
+//   let username = req.body.username;
+//   let password = req.body.password;
+//   User.findOne({ $or: [{ email: username }, { userName: username }] }).then(
+//     (user) => {
+//       console.log(user);
+//       if (user) {
+//         let result = bcrypt.compareSync(password, user.password);
+//         console.log(result);
+//         if (result) {
+//           res.json({
+//             message: "Log in successful",
+//           });
+//         } else {
+//           res.json({
+//             error: err.message,
+//           });
+//           console.log(user);
+//         }
+//       } else {
+//         res.json({
+//           message: "No user found",
+//         });
+//       }
+//     }
+//   );
+// };
+
+exports.signInAuthentication = function (req, res, next) {
+  let username = req.body.username;
+  let password = req.body.password;
+  User.findOne({ $or: [{ email: username }, { userName: username }] }).then(
+    async (user) => {
+      if (user) {
+        await bcrypt.compare(password, user.password, function (err, result) {
+          if (err) {
+            res.json({
+              error: err.message,
+            });
+            console.log(user.password);
+          }
+          if (result) {
+            res.json({
+              message: "Log in successful",
+            });
+          }
+        });
+      } else {
+        res.json({
+          message: "No user found",
+        });
+      }
     }
   );
-  e.preventDefault();
-  const data = await response.json();
-  console.log(data);
 };
